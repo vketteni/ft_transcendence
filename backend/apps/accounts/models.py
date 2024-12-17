dc24fab3f905b298465e1d14613ab3fb24367277
Ana V <138679587+anavoronkova@users.noreply.github.com>
2024-12-17 20:21:51 +0100

    Delete backend directory

--- backend/apps/accounts/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class User(AbstractUser):
-    # Add any additional fields if needed
-    pass
--- backend/apps/accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import UserProfileView
-
-urlpatterns = [
-    path('profile/', UserProfileView.as_view(), name='user-profile'),
-]
