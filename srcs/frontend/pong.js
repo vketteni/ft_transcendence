1a78eebb11e24d378dab81aebdca347cd4d01801
Anastasiia Voronkova <avoronko@c3a6c3.42berlin.de>
2025-01-22 14:03:50 +0100

    buttons.js + few frontend bugs

--- srcs/frontend/pong.js
+++ srcs/frontend/pong.js
@@ -1,23 +1,18 @@
 import { wsManager } from './WebSocketManager.js';
-import { sendAlias } from './sendToBackend.js';
-import { connectToMatchmaking, startPvCMatch } from './WebsocketMatchmaking.js';
 import { GAME_CONFIG, setPlayerID } from './config.js';
 import { resizeCanvas } from './render.js';
 import { DOM } from './dom.js';
-import { Timer } from './Timer.js';
-import { handleLoginRedirect, setLoginState } from './auth.js';
+import { setLoginState } from './auth.js';
 import { showScreen } from './showScreen.js';
-import { initializeSessionAndCSRF } from './intializeSessionAndCSRF.js';
 import { updateTopBar } from './topBar.js';
-import { fetchUserState } from './fetchUserState.js';
 import { handleLogout } from './logout.js';
-import { fetchGameData } from './token.js';
 import { getCookie, setCookie } from './cookie.js';
 import { generateUUID } from './generateUUID.js';
-
-let isPaused = false;
-const matchmakingTimer = new Timer(DOM.matchmakingTimer);
-const AItimer = new Timer(DOM.AItimer);
+import { Buttons } from './buttons.js';
+// import { connectToMatchmaking, startPvCMatch } from './WebsocketMatchmaking.js';
+// import { initializeSessionAndCSRF } from './intializeSessionAndCSRF.js';
+// import { fetchUserState } from './fetchUserState.js';
+// import { fetchGameData } from './token.js';
 
 DOM.canvas.width = GAME_CONFIG.canvasWidth;
 DOM.canvas.height = GAME_CONFIG.canvasHeight;
@@ -138,13 +133,6 @@ document.addEventListener('keyup', (e) => {
     }
 });
 
-// Stop and reset the timer when needed
-export function stopAndResetTimer() {
-    matchmakingTimer.stop();
-    matchmakingTimer.reset();
-}
-
-
 DOM.editProfileForm.addEventListener("submit", async (event) => {
 	event.preventDefault();
     const updatedData = {
@@ -186,6 +174,7 @@ DOM.editProfileForm.addEventListener("submit", async (event) => {
 });
 	
 document.addEventListener('DOMContentLoaded', () => {
+    Buttons.init(); 
 	// Set the category screen as the default
 	const screenId = location.hash.replace("#", "") || "category-screen";
     showScreen(screenId, false);
@@ -204,6 +193,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			} else if (sectionId === 'logout') {
 				handleLogout();
 				updateTopBar();
+                showScreen('category-screen');
 			} else if (sectionId === 'profile') {
 				showScreen('userprofile-screen');
 			} else {
@@ -224,113 +214,6 @@ setCookie('browser_id', generateUUID(), {
 	expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
 });
 localStorage.setItem('playerid', getCookie('browser_id'));
-
-// signup buttons
-DOM.registrationButton.addEventListener('click', () => {
-	console.log("registrationButton.addEventListener");
-	showScreen('signup-screen');
-});
-
-DOM.login42Button.addEventListener('click', () => {
-	console.log("login42Button.addEventListener()");
-	const loginWindow = window.open(
-		'/oauth/accounts/login/', // Redirects to backend endpoint for OAuth initiation
-		'_blank',          // Open in a new tab or popup
-		'width=500,height=600,noopener=false,noreferrer=false'
-	);
-	fetchUserState(loginWindow);
-	
-});
-
-DOM.editProfileButton.addEventListener("click", () => {
-	console.log("editProfileButton.addEventListener");
-	DOM.profileView.classList.add("d-none");
-	DOM.profileEdit.classList.remove("d-none");
-	
-	const profileData = {
-		username: DOM.profileUsername.textContent,
-		email: DOM.profileEmail.textContent,
-		first_name: DOM.profileFirstName.textContent,
-		last_name: DOM.profileLastName.textContent,
-	};
-	
-	DOM.editUsername.value = profileData.username;
-	DOM.editEmail.value = profileData.email;
-	DOM.editFirstName.value = profileData.first_name || 'N/A';
-	DOM.editLastName.value = profileData.last_name || 'N/A';
-});
-
-DOM.cancelEditButton.addEventListener("click", () => {
-	DOM.profileEdit.classList.add("d-none");
-	DOM.profileView.classList.remove("d-none");
-});
-
-//game play buttons
-DOM.PvCButton.addEventListener('click', () => {
-    console.log("PvC button clicked, showing matchmaking screen...");
-	showScreen('ai-waiting-screen');
-    AItimer.start();
-	connectToMatchmaking("PVC");
-});
-
-DOM.PvPButton.addEventListener('click', () => {
-    console.log("PvP button clicked, showing matchmaking screen...");
-    showScreen('matchmaking-screen');
-    matchmakingTimer.start();
-    connectToMatchmaking("PVP");
-});
-
-DOM.TournamentButton.addEventListener('click', () => {
-    console.log("Tournament button clicked, showing matchmaking screen...");
-	showScreen('matchmaking-screen');
-    AItimer.start();
-    startPvCMatch();
-});
-
-// DOM.pauseButton.addEventListener('click', () => {
-//     isPaused = !isPaused;
-
-//     if (isPaused) {
-//         DOM.pauseButton.classList.add('paused');
-//         DOM.pauseButton.textContent = "Resume";
-//         wsManager.send('game', { action: 'pause_game' });
-//     } else {
-//         DOM.pauseButton.classList.remove('paused');
-//         DOM.pauseButton.textContent = "Pause";
-//         wsManager.send('game', { action: 'resume_game' });
-//     }
-// });
-
-DOM.AIplayAgainButton.addEventListener('click', () => {
-    AItimer.start();
-    showScreen('ai-waiting-screen');
-	connectToMatchmaking("PVC");
-});
-
-DOM.PvPplayAgainButton.addEventListener('click', () => {
-    showScreen('matchmaking-screen');
-    matchmakingTimer.start();
-    connectToMatchmaking("PVP");
-});
-
-DOM.PvPbackToMenuButton.addEventListener('click', () => {
-    showScreen('category-screen');
-    // wsManager.close('game');
-});
-
-DOM.pauseButton.addEventListener('click', () => {
-    isPaused = !isPaused;
-
-    if (isPaused) {
-        DOM.pauseButton.classList.add('paused');
-        DOM.pauseButton.textContent = "Resume";
-        wsManager.send('game', { action: 'pause_game' });
-    } else {
-        DOM.pauseButton.classList.remove('paused');
-        DOM.pauseButton.textContent = "Pause";
-        wsManager.send('game', { action: 'resume_game' });
-    }
-});
 
 window.addEventListener("popstate", (event) => {
     if (event.state && event.state.screen) {
